5dfdc851f61ea71b9bf7f58bc6814e42bf9b57a8
metalspell <lonelyrockclimber@gmail.com>
2021-10-28 13:49:43 +0300

    Add new unit (redux & toolkit)

---
+++ unit_10/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import In from './In.js';
+import Out from './Out.js';
+
+function App() {
+  return (
+    <div className="App">
+      <In />
+      <Out />
+    </div>
+  );
+}
+
+export default App;
