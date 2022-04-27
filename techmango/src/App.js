d016fbcb31df492b323673a81499295183ba3e43
metalspell <lonelyrockclimber@gmail.com>
2022-04-27 13:28:56 +0300

    Initial commit

---
+++ techmango/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Mainpage from './Components/MainPage/Mainpage';
+
+function App() {
+  return (
+    <section className='main-wrapper'>
+      <Mainpage />
+    </section>
+  );
+}
+
+export default App;
---
+++ techmango/src/Components/MainPage/Mainpage.js
@@ -0,0 +1,11 @@
+import classes from './Mainpage.module.css';
+
+const Mainpage = () => {
+  return ( 
+    <section className={classes.mainWrapper}>
+
+    </section>
+  );
+}
+
+export default Mainpage;
