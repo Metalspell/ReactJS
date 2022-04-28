49a9bdd24bb1473d5d0ff648b8da62bca4ffe4bd
metalspell <lonelyrockclimber@gmail.com>
2022-04-28 15:44:42 +0300

    Finished main page

--- techmango/src/Components/MainPage/Mainpage.js
+++ techmango/src/Components/MainPage/Mainpage.js
@@ -1,9 +1,24 @@
 import classes from './Mainpage.module.css';
+import Button from '@mui/material/Button';
 
 const Mainpage = () => {
-  return ( 
+  return (
     <section className={classes.mainWrapper}>
-
+      <article className={classes.logoWrapper}>
+        <div className={classes.logo}></div>
+        <div className={classes.logoTitle}></div>
+      </article>
+      <article className={classes.joinUsWrapper}>
+        <h1 className={classes.joinUsTitle}>
+          Have <pre></pre> an idea?
+        </h1>
+        <div className={classes.buttonWrapper}>
+          <Button aria-label='submit' variant="contained" size='large' className={classes.button}>
+            CONTACT US
+          </Button>
+          <h2>We will do the rest!</h2>
+        </div>
+      </article>
     </section>
   );
 }
