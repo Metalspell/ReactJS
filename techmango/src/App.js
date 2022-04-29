58c5f6d2dfd84271db694e9ff91268b9ec2ed348
metalspell <lonelyrockclimber@gmail.com>
2022-04-29 16:29:55 +0300

    Add email field and start request

--- techmango/src/App.js
+++ techmango/src/App.js
@@ -4,6 +4,9 @@ import ModalWindow from './Components/ModalWindow/ModalWindow';
 import { useState } from "react";
 import Modal from "react-modal";
 import { useEffect } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,23 +18,25 @@ function App() {
   return (
     <section className='main-wrapper'>
       <Mainpage setIsOpen={setIsOpen} />
-      <Modal
-        ariaHideApp={false}
-        setIsOpen={setIsOpen}
-        isOpen={isOpen}
-        overlayClassName={{
-          base: "overlay-base",
-          afterOpen: "overlay-after",
-          beforeClose: "overlay-before"
-        }}
-        className={{
-          base: "content-base",
-          afterOpen: "content-after",
-          beforeClose: "content-before"
-        }}
-      >
-        <ModalWindow setIsOpen={setIsOpen} />
-      </Modal>
+      <QueryClientProvider client={queryClient}>
+        <Modal
+          ariaHideApp={false}
+          setIsOpen={setIsOpen}
+          isOpen={isOpen}
+          overlayClassName={{
+            base: "overlay-base",
+            afterOpen: "overlay-after",
+            beforeClose: "overlay-before"
+          }}
+          className={{
+            base: "content-base",
+            afterOpen: "content-after",
+            beforeClose: "content-before"
+          }}
+        >
+          <ModalWindow setIsOpen={setIsOpen} />
+        </Modal>
+      </QueryClientProvider>
     </section>
   );
 }
--- techmango/src/Components/ModalWindow/ModalWindow.js
+++ techmango/src/Components/ModalWindow/ModalWindow.js
@@ -1,14 +1,75 @@
 import './Modal.css';
 import Logo from '../Logo/Logo';
+import { useState } from "react";
+import axios from 'axios';
+import { useMutation } from 'react-query';
+import Button from '@mui/material/Button';
+import * as yup from 'yup';
+import { useFormik } from 'formik';
+
+const API_URL = 'https://api.qa.zgambling.com/public/password_recovery';
+
+
+const validationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email(`Please enter a valid email`)
+    .required(`Required field`),
+});
 
 const ModalWindow = ({ setIsOpen }) => {
 
-  return (
-    <div className="modal-body">
-      <Logo />
-      <img className="modal-close-icon" onClick={() => setIsOpen(false)} src={require('./icons/Group110.png')} alt="" />
-    </div>
-  );
+  const [request, setrequest] = useState(null);
+  const { mutateAsync, isLoading } = useMutation((data) => axios.post(API_URL, JSON.stringify(data)));
+  const handleSubmit = async (values, { resetForm }) => {
+    await mutateAsync(values, {
+      onSuccess: () => {
+        setrequest(true);
+        resetForm();
+      },
+      onError: () => {
+        setrequest(false);
+        resetForm();
+      },
+    });
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+    },
+    onSubmit: handleSubmit,
+  });
+
+  if (request === null) {
+    return (
+      <div className="modal-body">
+        <Logo />
+        <img className="modal-close-icon"
+          onClick={() => setIsOpen(false)}
+          src={require('./icons/Group110.png')}
+          alt=""
+        />
+
+        <form onSubmit={formik.handleSubmit}>
+          <label htmlFor="email">Email Address</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+  if (request === true) {
+    return (
+      <h1>Hhhhhhhhhhhhhhhhhhhhhhhhhh</h1>
+    )
+  }
 }
 
 export default ModalWindow;
